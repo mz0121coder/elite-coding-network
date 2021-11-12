6bbd9038f24b70b7285227a917a163caac191aa4
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-12 22:14:19 +0000

    Export follow (async)

--- utils/profileMethods.js
+++ utils/profileMethods.js
@@ -8,3 +8,16 @@ const Axios = axios.create({
   mainUrl: `${mainUrl}/api/profile`,
   headers: { Authorization: cookie.get("token") },
 });
+
+export const follow = async (followUserId, setUserFollowers) => {
+    try {
+      await Axios.post(`/follow/${followUserId}`);
+  
+      setUserFollowers((prev) => ({
+        ...prev,
+        following: [...prev.following, { user: followUserId }],
+      }));
+    } catch (error) {
+      alert(catchErrors(error));
+    }
+  };
