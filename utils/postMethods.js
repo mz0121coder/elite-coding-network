ab5a3ee711246ac840beca779ee0e1c5f42687c8
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-12 22:05:32 +0000

    Submit new post functionality

--- utils/postMethods.js
+++ utils/postMethods.js
@@ -7,3 +7,22 @@ export const Axios = axios.create({
   mainUrl: `${mainUrl}/api/posts`,
   headers: { Authorization: cookie.get("token") },
 });
+
+export const submitNewPost = async (
+    text,
+    location,
+    picUrl,
+    setPosts,
+    setNewPost,
+    setError
+  ) => {
+    try {
+      const res = await Axios.post("/", { text, location, picUrl });
+  
+      setPosts((prev) => [res.data, ...prev]);
+      setNewPost({ text: "", location: "" });
+    } catch (error) {
+      const errorMsg = catchErrors(error);
+      setError(errorMsg);
+    }
+  };
