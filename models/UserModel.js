6e28eafeab1e95621f82602e4f688a6946a8922a
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-14 16:30:57 +0500

    First batch of objects in user schema

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema();
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+
+  email: { type: String, required: true, unique: true },
+
+  password: { type: String, required: true, select: false },
+
+  username: { type: String, required: true, unique: true, trim: true },
+
+  dpLink: { type: String },
+
+  newMessageAlert: { type: Boolean, default: true },
+});
