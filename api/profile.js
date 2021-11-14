d9711ff6867d0d166f7f4e4439024269197edc45
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-14 20:06:57 +0000

    Get posts of user

--- api/profile.js
+++ api/profile.js
@@ -41,3 +41,27 @@ router.get("/:username", authMiddleware, async (req, res) => {
       return res.status(500).send("Server Error");
     }
   });
+
+  // GET POSTS OF USER
+router.get(`/posts/:username`, authMiddleware, async (req, res) => {
+    try {
+      const { username } = req.params;
+  
+      const user = await UserModel.findOne({ username: username.toLowerCase() });
+      if (!user) {
+        return res.status(404).send("No User Found");
+      }
+  
+      const posts = await PostModel.find({ user: user._id })
+        .sort({ createdAt: -1 })
+        .populate("user")
+        .populate("comments.user");
+  
+      return res.json(posts);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Server Error");
+    }
+  });
+  
+  
