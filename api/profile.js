e7f924c18a2df73b4338f80e0cdefe3a26cdacd0
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-14 20:05:24 +0000

    Get profile info

--- api/profile.js
+++ api/profile.js
@@ -10,3 +10,34 @@ const {
   newFollowerAlert,
   deleteFollowerAlert,
 } = require("../utilsServer/notifs");
+
+// GET PROFILE INFO
+router.get("/:username", authMiddleware, async (req, res) => {
+    try {
+      const { username } = req.params;
+  
+      const user = await UserModel.findOne({ username: username.toLowerCase() });
+      if (!user) {
+        return res.status(404).send("No User Found");
+      }
+  
+      const profile = await ProfileModel.findOne({ user: user._id }).populate(
+        "user"
+      );
+  
+      const followerStats = await FollowerModel.findOne({ user: user._id });
+  
+      return res.json({
+        profile,
+  
+        numberOfFollowers:
+          followerStats.followers.length > 0 ? followerStats.followers.length : 0,
+  
+        numberFollowing:
+          followerStats.following.length > 0 ? followerStats.following.length : 0,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Server Error");
+    }
+  });
