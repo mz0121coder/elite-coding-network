3a2cfed6621be963e97dcdd797841169fa7c2dbf
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-14 19:44:00 +0000

    Routes for creating post

--- api/posts.js
+++ api/posts.js
@@ -11,3 +11,30 @@ const {
   newCommentAlert,
   deleteCommentAlert,
 } = require("../utilsServer/notifs");
+
+// CREATE A POST
+
+router.post("/", authMiddleware, async (req, res) => {
+    const { text, location, picUrl } = req.body;
+  
+    if (text.length < 1)
+      return res.status(401).send("Text must be at least 1 character");
+  
+    try {
+      const newPost = {
+        user: req.userId,
+        text,
+      };
+      if (location) newPost.location = location;
+      if (picUrl) newPost.picUrl = picUrl;
+  
+      const post = await new PostModel(newPost).save();
+  
+      const postAdded = await PostModel.findById(post._id).populate("user");
+  
+      return res.json(postAdded);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send(`Server error`);
+    }
+  });
