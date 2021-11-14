dc3b005881a7224841626b1d49aeb9635811a690
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-14 19:41:44 +0000

    Dependencies for posts

---
+++ api/posts.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const UserModel = require("../models/UserModel");
+const PostModel = require("../models/PostModel");
+const FollowerModel = require("../models/FollowerModel");
+const uuid = require("uuid").v4;
+const {
+  newLikeAlert,
+  deleteLikeAlert,
+  newCommentAlert,
+  deleteCommentAlert,
+} = require("../utilsServer/notifs");
