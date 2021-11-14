ae66cb403dad3feac34b138fd5dc760b2e45b570
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-14 16:29:26 +0500

    Set timestamps to true, export profile schema

--- models/ProfileModel.js
+++ models/ProfileModel.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProfileSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
+const ProfileSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User" },
 
-  bio: { type: String, required: true },
+    bio: { type: String, required: true },
 
-  social: {
-    github: { type: String },
-    connectdevelop: { type: String },
-    at: { type: String },
-    linkify: { type: String },
+    social: {
+      github: { type: String },
+      connectdevelop: { type: String },
+      at: { type: String },
+      linkify: { type: String },
+    },
   },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Profile", ProfileSchema);
