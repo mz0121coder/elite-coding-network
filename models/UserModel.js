8550fb29106319ff7f91bf7375542a866197d436
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-14 16:32:21 +0500

    Set timestamps true, export UserSchema

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: { type: String, required: true },
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
 
-  email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
 
-  password: { type: String, required: true, select: false },
+    password: { type: String, required: true, select: false },
 
-  username: { type: String, required: true, unique: true, trim: true },
+    username: { type: String, required: true, unique: true, trim: true },
 
-  dpLink: { type: String },
+    dpLink: { type: String },
 
-  newMessageAlert: { type: Boolean, default: true },
+    newMessageAlert: { type: Boolean, default: true },
 
-  unreadMsg: { type: Boolean, default: false },
+    unreadMsg: { type: Boolean, default: false },
 
-  unreadNotification: { type: Boolean, default: false },
+    unreadNotification: { type: Boolean, default: false },
 
-  role: { type: String, default: "user", enum: ["user", "root"] },
+    role: { type: String, default: "user", enum: ["user", "root"] },
 
-  resetToken: { type: String },
+    resetToken: { type: String },
 
-  expireToken: { type: Date },
-});
+    expireToken: { type: Date },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
