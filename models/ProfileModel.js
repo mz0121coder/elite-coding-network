d8a3d1c12a920b05f2c96242c7be2bb681f744b4
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-14 16:28:31 +0500

    Build schema to link github + 3 other social media

--- models/ProfileModel.js
+++ models/ProfileModel.js
@@ -1,4 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProfileSchema = new Schema();
+const ProfileSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+
+  bio: { type: String, required: true },
+
+  social: {
+    github: { type: String },
+    connectdevelop: { type: String },
+    at: { type: String },
+    linkify: { type: String },
+  },
+});
