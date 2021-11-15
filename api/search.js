99416b9c4931f5476037b5efe2d38ee0009a0c94
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-15 18:12:18 +0500

    Get search input, include regex and user id

--- api/search.js
+++ api/search.js
@@ -2,3 +2,23 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const UserModel = require("../models/UserModel");
+
+router.get("/:searchInput", authMiddleware, async (req, res) => {
+  try {
+    const { searchInput } = req.params;
+    const { userId } = req;
+    if (searchInput.length === 0) return;
+
+    const results = await UserModel.find({
+      name: { $regex: searchInput, $options: "i" },
+      $nor: [{ _id: userId }],
+    });
+
+    return res.status(200).json(results);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
+module.exports = router;
