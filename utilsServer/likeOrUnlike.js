0645496ec05607f6b912402ad2b17ea6e66f4956
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-16 20:30:44 +0000

    edit syntax issue

--- utilsServer/likeOrUnlike.js
+++ utilsServer/likeOrUnlike.js
@@ -1,5 +1,9 @@
 const UserModel = require ("../models/UserModel");
-const likeOrUnlike = async (postId, UserId, like) = {
+const PostModel = require("../models/PostModel");
+const { newLikeAlert, deleteLikeAlert } = require("../utilsServer/notifs");
+
+
+const likeOrUnlike = async (postId, UserId, like) => {
     try {
         const post = await PostModel.findbyId (postId);
 
@@ -7,13 +11,61 @@ const likeOrUnlike = async (postId, UserId, like) = {
 
         if (like) {
             const isLiked =
-            post.likes.filter((like)) => like.user.tostring() === userId.length >0;
+            post.likes.filter((like) => like.user.tostring() === userId).length >0;
 
-            if (isLiked) return { error: Post liked before"};
+            if (isLiked) return { error: "Post liked before"};
 
             await post.likes.unshift({ user: userId});
-        }
-    }
-}
-}
+            
+            await post.save ();
 
+            if (post.user.toString () !== userId) {
+                await newLikeAlert(userId, postId, post.user.toString());
+
+            }
+        }
+        else {
+            const isLiked =
+            post.likes.filter((like) => like.user.toString() === userId).length === 0;
+            
+            if (isLiked) return {error: "Post not liked before"};
+
+            const indexOf = post.likes
+            .map (( like) => like.user.toString())
+            .indexOf(userId);
+
+            await post.likes.splice(indexOf, 1);  
+
+            await post.save();
+
+            if (post.save());
+
+            if (post.user.toString()!== userId) {
+                await deleteLikeAlert (userId, postId, post.user.toString());
+            }
+        }
+
+        const user =await UserModel.findById(UserId);
+
+        const {name, dpLink, username } = user;
+
+        return {
+            success: true,
+            name,
+            dpLink,
+            username, 
+            postByUserId: post.user.toString(),
+        };
+    } catch (error) {
+        return { error: "Server Error "};
+    }
+};
+
+module.exports = { likeOrUnlike };
+
+
+
+
+   
+
+    
