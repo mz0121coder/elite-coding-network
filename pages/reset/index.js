16dde352e430f0a9db33d727ec545fab64e30fd7
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-17 01:18:17 +0500

    Dependencies for index.js in reset subfolder

---
+++ pages/reset/index.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { Form, Button, Message, Segment } from "semantic-ui-react";
+import mainUrl from "../../utils/mainUrl";
+import catchErrors from "../../utils/catchErrors";
+import axios from "axios";
