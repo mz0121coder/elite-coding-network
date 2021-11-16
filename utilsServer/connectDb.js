9fc37514657dc73a1db937693d0b1b674a104dea
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-16 16:02:51 +0000

    create connections for db

---
+++ utilsServer/connectDb.js
@@ -0,0 +1,15 @@
+const mongoose =  require ("mongoose");
+
+async await connect() {  //tutor session
+    try {
+    await useNewParser: true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    });
+
+
+}
+
+module.exports = connectDB;
