8e3725a54704ceeae165ffe70139e725ca8e0417
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-16 16:10:42 +0000

    correct error

--- utilsServer/connectDb.js
+++ utilsServer/connectDb.js
@@ -1,6 +1,6 @@
 const mongoose =  require ("mongoose");
 
-async function connectDB() {  //tutor session
+async function connectDb() {  //tutor session
     try {
     await mongoose.connect (process.env.MONGO_URI, {
     useNewUrlParser:true,
