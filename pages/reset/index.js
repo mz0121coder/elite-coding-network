609b7271e61200a4ff2244635264a31cfc36a357
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-17 01:29:46 +0500

    Reset function with try-catch block

--- pages/reset/index.js
+++ pages/reset/index.js
@@ -3,3 +3,28 @@ import { Form, Button, Message, Segment } from "semantic-ui-react";
 import mainUrl from "../../utils/mainUrl";
 import catchErrors from "../../utils/catchErrors";
 import axios from "axios";
+
+function ResetPage() {
+    const [email, setEmail] = useState("");
+    const [errorMsg, setErrorMsg] = useState(null);
+  
+    const [emailChecked, setEmailChecked] = useState(false);
+  
+    const [loading, setLoading] = useState(false);
+  
+    const resetPassword = async (e) => {
+      e.preventDefault();
+  
+      setLoading(true);
+  
+      try {
+        await axios.post(`${mainUrl}/api/reset`, { email });
+  
+        setEmailChecked(true);
+      } catch (error) {
+        setErrorMsg(catchErrors(error));
+      }
+  
+      setLoading(false);
+    };
+  
