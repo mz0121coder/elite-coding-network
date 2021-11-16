ff18a46f01613c60ab285b3b973ea68ac0ec46af
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-17 01:15:00 +0500

    TokenPage and handleChange function(1)

--- pages/reset/[token].js
+++ pages/reset/[token].js
@@ -4,3 +4,21 @@ import { Form, Button, Message, Segment, Divider } from "semantic-ui-react";
 import mainUrl from "../../utils/mainUrl";
 import catchErrors from "../../utils/catchErrors";
 import axios from "axios";
+
+function TokenPage() {
+    const router = useRouter();
+  
+    const [newPassword, setNewPassword] = useState({ field1: "", field2: "" });
+  
+    const { field1, field2 } = newPassword;
+  
+    const [loading, setLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [success, setSuccess] = useState(false);
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+  
+      setNewPassword((prev) => ({ ...prev, [name]: value }));
+    };
+  
