212bf19310087771dcf5476a059021ff9947e4c8
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-17 01:13:13 +0500

    Return frontend for posts

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -19,13 +19,106 @@ import mainUrl from "../../utils/mainUrl";
 import { NoPostListed } from "../../components/Layout/NoData";
 
 function PostPage({ post, errorLoading, user }) {
-    if (errorLoading) {
-      return <NoPostListed />;
-    }
-  
-    const [likes, setLikes] = useState(post.likes);
-  
-    const isLiked =
-      likes.length > 0 &&
-      likes.filter((like) => like.user === user._id).length > 0;
-  
+  if (errorLoading) {
+    return <NoPostListed />;
+  }
+
+  const [likes, setLikes] = useState(post.likes);
+
+  const isLiked =
+    likes.length > 0 &&
+    likes.filter((like) => like.user === user._id).length > 0;
+
+  const [comments, setComments] = useState(post.comments);
+
+  return (
+    <Container text>
+      <Segment basic>
+        <Card color="teal" fluid>
+          {post.picUrl && (
+            <Image
+              src={post.picUrl}
+              style={{ cursor: "pointer" }}
+              floated="left"
+              wrapped
+              ui={false}
+              alt="PostImage"
+              onClick={() => setShowModal(true)}
+            />
+          )}
+
+          <Card.Content>
+            <Image floated="left" src={post.user.dpLink} avatar circular />
+            <Card.Header>
+              <Link href={`/${post.user.username}`}>
+                <a>{post.user.name}</a>
+              </Link>
+            </Card.Header>
+
+            <Card.Meta>{calcTime(post.createdAt)}</Card.Meta>
+
+            {post.location && <Card.Meta content={post.location} />}
+
+            <Card.Description
+              style={{
+                fontSize: "17px",
+                letterSpacing: "0.1px",
+                wordSpacing: "0.35px",
+              }}
+            >
+              {post.text}
+            </Card.Description>
+          </Card.Content>
+
+          <Card.Content extra>
+            <Icon
+              name={isLiked ? "heart" : "heart outline"}
+              color="red"
+              style={{ cursor: "pointer" }}
+              onClick={() =>
+                likePost(post._id, user._id, setLikes, isLiked ? false : true)
+              }
+            />
+
+            <LikesList
+              postId={post._id}
+              trigger={
+                likes.length > 0 && (
+                  <span className="spanLikesList">
+                    {`${likes.length} ${likes.length === 1 ? "like" : "likes"}`}
+                  </span>
+                )
+              }
+            />
+
+            <Icon
+              name="comment outline"
+              style={{ marginLeft: "7px" }}
+              color="blue"
+            />
+
+            {comments.length > 0 &&
+              comments.map((comment) => (
+                <AddComments
+                  key={comment._id}
+                  comment={comment}
+                  postId={post._id}
+                  user={user}
+                  setComments={setComments}
+                />
+              ))}
+
+            <Divider hidden />
+
+            <CommentTab
+              user={user}
+              postId={post._id}
+              setComments={setComments}
+            />
+          </Card.Content>
+        </Card>
+      </Segment>
+      <Divider hidden />
+    </Container>
+  );
+}
