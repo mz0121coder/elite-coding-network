e49d060a49acae79f8ef248c5d4d113c0cdedaac
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-16 19:13:42 +0000

    Login and handleChange function

--- pages/login.js
+++ pages/login.js
@@ -3,3 +3,23 @@ import { Form, Button, Message, Segment, Divider } from "semantic-ui-react";
 import { loginUser } from "../utils/authUser";
 import { HeaderMessage, FooterMessage } from "../components/Common/WelcomeMsg";
 import cookie from "js-cookie";
+
+function Login() {
+    const [user, setUser] = useState({
+      email: "",
+      password: "",
+    });
+  
+    const { email, password } = user;
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [formLoading, setFormLoading] = useState(false);
+    const [submitDisabled, setSubmitDisabled] = useState(true);
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+  
+      setUser((prev) => ({ ...prev, [name]: value }));
+    };
+  
+  
