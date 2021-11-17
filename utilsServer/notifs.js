043cb3a1a6e2cdcf407a2dc61d8042cd5c8adcc4
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-17 00:24:30 +0000

    set up like alert

--- utilsServer/notifs.js
+++ utilsServer/notifs.js
@@ -1,4 +1,20 @@
 const UserModel = require("../modelsUserModel");
 const NotificationModel = request ("..models/NotificationModel");
 
-const setAlertToUnread = async (userId => {})
+const setAlertToUnread = async (userId => {
+    try {
+        const user = await UserModel.findById (userId);
+
+        if (!user.unreadNotification){
+            user.unreadNotification = true;
+            await user.save();
+        }
+
+        return;
+    }catch (error) {
+        console.error(error);
+    }
+};
+
+const newLikeAlert = async (userId, postId, userToNotifySS)
+});
