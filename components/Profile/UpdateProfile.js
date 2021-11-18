d2dc7e60feeabc84fb59abe041cdcb61fdb1d538
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 20:16:21 +0000

    UpdateProfile options (dp, bio, github, socials)

--- components/Profile/UpdateProfile.js
+++ components/Profile/UpdateProfile.js
@@ -4,3 +4,15 @@ import ImageFormat from "../Common/ImageFormat";
 import CommonFields from "../Common/CommonFields";
 import uploadPic from "../../utils/Cloudinary";
 import { profileUpdate } from "../../utils/profileMethods";
+
+function UpdateProfile({ Profile }) {
+    const [profile, setProfile] = useState({
+      dpLink: Profile.user.dpLink,
+      bio: Profile.bio || "",
+      github: (Profile.social && Profile.social.github) || "",
+      at: (Profile.social && Profile.social.at) || "",
+      linkify: (Profile.social && Profile.social.linkify) || "",
+      connectdevelop: (Profile.social && Profile.social.connectdevelop) || "",
+    });
+  
+    
