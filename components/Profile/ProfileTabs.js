be6eadcda972824cc2a315acee9f58e48b8db197
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 19:53:19 +0000

    ProfileTabs function

--- components/Profile/ProfileTabs.js
+++ components/Profile/ProfileTabs.js
@@ -1,2 +1,12 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+
+function ProfileTabs({
+    activeComponent,
+    handleComponent,
+    numberOfFollowers,
+    numberFollowing,
+    userAccount,
+    loggedUserFollowers,
+  }) {
+  
