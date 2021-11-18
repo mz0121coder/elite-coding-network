1886bb0d69864091784b988bf14b7f4f58c2cb78
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-18 02:21:24 +0000

    add email auth route

--- api/auth.js
+++ api/auth.js
@@ -7,22 +7,30 @@ const FollowerModel = require ("../models/FollowerModel");
 const UserModel = require ("..models/UserModel");
 
 
-router.get ("/"), authMiddleware, async (req,res) =>{
-    const {userId} =req;
+router.get ("/"), authMiddleware, async (req,res) => {
+    const {userId} = req;
 
     try {
         const user = await UserModel.findById (userId);
         if (!user) {
-            return res.status(404).send ("User Not Found");
-        }
-    }
+            return res.status(404).send ("User Not Found :( ");
 }
 
-const userFollowers = await FollowerModel.findOne({user:userId});
+const userFollowers = await FollowerModel.findOne({ user:userId });
 
 return res.status (200).json({user, userFollowers });
 } catch (error) {
     console.error(error);
-    return res.status(500.send ("Server Error");
+    return res.status(500.send ("Server Sleeping - Try Again!");
 }
 });
+
+router.post ("/"), async (req,res)  => {
+    const {email, password } = req.body.user;
+    
+    if (!isEmail (email)) return res.status (401).send ("Invalid Email");
+
+    if (password.length < ????????6) {
+        return res.status 
+    }
+}
