04618cf6d3436538b27d96054fdc5062e057480f
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-18 18:00:32 +0000

    add jwt payload

--- api/auth.js
+++ api/auth.js
@@ -5,6 +5,7 @@ const router = express.Router ();
 const authMiddleware = require ("../middleware/authMiddleware");
 const FollowerModel = require ("../models/FollowerModel");
 const UserModel = require ("..models/UserModel");
+const { JsonWebTokenError } = require("jsonwebtoken");
 
 
 router.get ("/"), authMiddleware, async (req,res) => {
@@ -30,8 +31,31 @@ router.post ("/"), async (req,res)  => {
     
     if (!isEmail (email)) return res.status (401).send ("Invalid Email");
 
-    if (password.length < ????????6) {
-        return res.status 
+    if (password.length < 8) {
+        return res.status (401).send ("Your password must be atleast 8 characters long");
+    }
+
+    try {
+        const user + await UserModel.findOne({email: email.toLowerCase()}).select (
+            "+password"
+        );
+
+        if (!user) {
+            return res.status (401).send ("Not recognised");
+        }
+
+        //encryption???
+
+        const payload = { userId: user._id };
+        jwt.sign(
+            payload,
+            process.env.jwtSecret,
+            {expiresIn: 24hours},
+            (err,token) => {
+                if (err) throw err;
+                res.status(200).json(token);
+            }
+        );
     }
 }
 
