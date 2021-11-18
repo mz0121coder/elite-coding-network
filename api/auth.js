989ae255a298567d5a6b1b99ca5b6b6c41959794
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-18 18:46:09 +0000

    text formatting (auth) + error check

--- api/auth.js
+++ api/auth.js
@@ -1,66 +1,68 @@
-const express = require ("express");
+const express = require("express");
+const router = express.Router();
+const UserModel = require("../models/UserModel");
+const FollowerModel = require("../models/FollowerModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const isEmail = require("validator/lib/isEmail");
+const authMiddleware = require("../middleware/authMiddleware");
 
-const router = express.Router ();
+router.get("/", authMiddleware, async (req, res) => {
+  const { userId } = req;
 
-const authMiddleware = require ("../middleware/authMiddleware");
-const FollowerModel = require ("../models/FollowerModel");
-const UserModel = require ("..models/UserModel");
-const { JsonWebTokenError } = require("jsonwebtoken");
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
 
+    const userFollowers = await FollowerModel.findOne({ user: userId });
 
-router.get ("/"), authMiddleware, async (req,res) => {
-    const {userId} = req;
-
-    try {
-        const user = await UserModel.findById (userId);
-        if (!user) {
-            return res.status(404).send ("User Not Found :( ");
-}
-
-const userFollowers = await FollowerModel.findOne({ user:userId });
-
-return res.status (200).json({ user, userFollowers });
-} catch (error) {
+    return res.status(200).json({ user, userFollowers });
+  } catch (error) {
     console.error(error);
-    return res.status(500).send ("Server Sleeping - Try Again!");
-}
+    return res.status(500).send(`Server error`);
+  }
 });
 
-router.post ("/"), async (req,res)  => {
-    const {email, password } = req.body.user;
-    
-    if (!isEmail (email)) return res.status (401).send ("Invalid Email");
+router.post("/", async (req, res) => {
+  const { email, password } = req.body.user;
 
-    if (password.length < 8) {
-        return res.status (401).send ("Your password must be atleast 8 characters long");
+  if (!isEmail(email)) return res.status(401).send("Invalid Email");
+
+  if (password.length < 6) {
+    return res.status(401).send("Password must be at least 6 characters");
+  }
+
+  try {
+    const user = await UserModel.findOne({ email: email.toLowerCase() }).select(
+      "+password"
+    );
+
+    if (!user) {
+      return res.status(401).send("Invalid Credentials");
     }
 
-    try {
-        const user = await UserModel.findOne({email: email.toLowerCase()}).select (
-            "+password"
-        );
-
-        if (!user) {
-            return res.status (401).send ("Not recognised");
-        }
-
-        //encryption???
-
-        const payload = { userId: user._id };
-        jwt.sign(
-            payload,
-            process.env.jwtSecret,
-            {expiresIn: "24hours" },
-            (err,token) => {
-                if (err) throw err;
-                res.status(200).json(token);
-            }
-        );
-    } catch (error) {
-        console.error (error);
-        return res.status (500).send ("Server Sleeping - Try again!");
+    const usingPassword = await bcrypt.compare(password, user.password);
+    if (!usingPassword) {
+      return res.status(401).send("Invalid Credentials");
     }
-};
+
+    const payload = { userId: user._id };
+    jwt.sign(
+      payload,
+      process.env.jwtSecret,
+      { expiresIn: "2d" },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json(token);
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
 
 module.exports = router;
 
