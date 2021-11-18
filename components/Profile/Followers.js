d0728a9d98c22a7fa42eaace06e6f7ff34080392
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 19:38:50 +0000

    Return frontend w/ spinEffect and followers info

--- components/Profile/Followers.js
+++ components/Profile/Followers.js
@@ -37,4 +37,58 @@ const Followers = ({
   
       getFollowers();
     }, []);
+
+    return (
+        <>
+          {loading ? (
+            <SpinEffect />
+          ) : followers.length > 0 ? (
+            followers.map((profileFollower) => {
+              /*  */
+    
+              const isFollowingUser =
+                loggedUserFollowers.following.length > 0 &&
+                loggedUserFollowers.following.filter(
+                  (following) => following.user === profileFollower.user._id
+                ).length > 0;
+    
+              return (
+                <List key={profileFollower.user._id} divided verticalAlign="middle">
+                  <List.Item>
+                    <List.Content floated="right">
+                      {profileFollower.user._id !== user._id && (
+                        <Button
+                          color={isFollowingUser ? "linkify" : "connectdevelop"}
+                          icon={isFollowingUser ? "check" : "add user"}
+                          content={isFollowingUser ? "Following" : "Follow"}
+                          disabled={loadFollowInfo}
+                          onClick={() => {
+                            setLoadFollowInfo(true);
+    
+                            isFollowingUser
+                              ? unfollow(profileFollower.user._id, setUserFollowers)
+                              : follow(profileFollower.user._id, setUserFollowers);
+    
+                            setLoadFollowInfo(false);
+                          }}
+                        />
+                      )}
+                    </List.Content>
+                    <Image avatar src={profileFollower.user.dpLink} />
+                    <List.Content as="a" href={`/${profileFollower.user.username}`}>
+                      {profileFollower.user.name}
+                    </List.Content>
+                  </List.Item>
+                </List>
+              );
+            })
+          ) : (
+            <NoFollowInfo followersTab={true} />
+          )}
+        </>
+      );
+    };
+    
+    export default Followers;
+    
   
