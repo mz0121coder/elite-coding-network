794f71fe5a3c1be42c7ff86177d3ecd0fb4bee74
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 19:57:07 +0000

    Menu items for profile choices

--- components/Profile/ProfileTabs.js
+++ components/Profile/ProfileTabs.js
@@ -10,3 +10,56 @@ function ProfileTabs({
     loggedUserFollowers,
   }) {
   
+    return (
+        <>
+          <Menu pointing secondary>
+            <Menu.Item
+              name="profile"
+              active={activeComponent === "profile"}
+              onClick={() => handleComponent("profile")}
+            />
+    
+            <Menu.Item
+              name={`${numberOfFollowers} followers`}
+              active={activeComponent === "followers"}
+              onClick={() => handleComponent("followers")}
+            />
+    
+            {userAccount ? (
+              <>
+                <Menu.Item
+                  name={`${
+                    loggedUserFollowers.following.length > 0
+                      ? loggedUserFollowers.following.length
+                      : 0
+                  } following`}
+                  active={activeComponent === "following"}
+                  onClick={() => handleComponent("following")}
+                />
+    
+                <Menu.Item
+                  name="Update Profile"
+                  active={activeComponent === "updateProfile"}
+                  onClick={() => handleComponent("updateProfile")}
+                />
+    
+                <Menu.Item
+                  name="settings"
+                  active={activeComponent === "settings"}
+                  onClick={() => handleComponent("settings")}
+                />
+              </>
+            ) : (
+              <Menu.Item
+                name={`${numberFollowing} following`}
+                active={activeComponent === "following"}
+                onClick={() => handleComponent("following")}
+              />
+            )}
+          </Menu>
+        </>
+      );
+    }
+    
+    export default ProfileTabs;
+    
