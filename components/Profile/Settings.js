165c886738e120b5a3aefe012f6f266569482d54
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 20:00:32 +0000

    Imports for settings.js

---
+++ components/Profile/Settings.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from "react";
+import {
+  List,
+  Divider,
+  Message,
+  Checkbox,
+  Form,
+  Button,
+} from "semantic-ui-react";
+import { pwUpdate, toggleMsgAlert } from "../../utils/profileMethods";
