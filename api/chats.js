304cdef0f4e361b9e299a870456770152c540c7b
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-18 19:37:19 +0000

    install chat dependencies

--- api/chats.js
+++ api/chats.js
@@ -1,10 +1,28 @@
 const express = require ("express");
 
 const router = express.Router ();
-const authMiddleware = require ("../middleware/authMiddleware");
 const ChatModel = require ("../models/ChatModel");
 const UserModel = require ("../models/UserModel");
+const authMiddleware = require ("../middleware/authMiddleware");
 
+router.get("/", authMiddleware, async (req, res) => {
+    try {
+        const { userId } = req;
 
+        const user = await ChatModel.findOne ({user: userId }).populate (
+            "chats.msgWithUser"
+        );
 
+        let chatsToSend = [];
+
+        if (user.chats.length > 0) {
+            chatsToSend = await user.chats.map((chat) => ({
+                msgsWithUser: chat.msgsWithUser._id,
+                name: chat.msgWithUser.name,
+                dpLink: chat.msgsWithUser.dpLink,
+                lastMessage: chat.messages[chat.messages.length -1].msg,
+                date: chat.messages [chat.messages.length -1].date,
+            }));
+        }
+    }
 
