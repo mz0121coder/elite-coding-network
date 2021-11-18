1c17b21a568a546af78f07ab2e1a4a1813d492b7
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 20:02:13 +0000

    Settings function, useRef and useState(1)

--- components/Profile/Settings.js
+++ components/Profile/Settings.js
@@ -8,3 +8,15 @@ import {
   Button,
 } from "semantic-ui-react";
 import { pwUpdate, toggleMsgAlert } from "../../utils/profileMethods";
+
+function Settings({ newMessageAlert }) {
+    const [passwordFields, showPasswordFields] = useState(false);
+  
+    const [newMessageSettings, showNewMessageSettings] = useState(false);
+  
+    const isFirstRun = useRef(true);
+    const [popupSetting, setPopupSetting] = useState(newMessageAlert);
+  
+    const [success, setSuccess] = useState(false);
+  
+  
