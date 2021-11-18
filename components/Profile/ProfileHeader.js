c48a89835334bea5d701842af8a93013e0bed0ff
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 19:47:17 +0000

    ProfileHeader function, useState(false)

--- components/Profile/ProfileHeader.js
+++ components/Profile/ProfileHeader.js
@@ -9,3 +9,19 @@ import {
   List,
 } from "semantic-ui-react";
 import { follow, unfollow } from "../../utils/profileMethods";
+
+function ProfileHeader({
+    profile,
+    userAccount,
+    loggedUserFollowers,
+    setUserFollowers,
+  }) {
+    const [loading, setLoading] = useState(false);
+  
+    const isFollowingUser =
+      loggedUserFollowers.following.length > 0 &&
+      loggedUserFollowers.following.filter(
+        (following) => following.user === profile.user._id
+      ).length > 0;
+  
+  
