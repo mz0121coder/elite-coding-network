5267de466ca3367ba6d5201d2af56285d94139da
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-18 01:02:20 +0000

    require routes

---
+++ api/auth.js
@@ -0,0 +1,7 @@
+const express = require ("express");
+
+const router = express.Router ();
+
+const UserModel = require ("..models/UserModel");
+const FollowerModel = require ("../models/FollowerModel");
+
