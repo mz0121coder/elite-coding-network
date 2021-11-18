3a99124498fc134f6049c992c1b64fc567632c6a
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 19:44:49 +0000

    Return frontend for following tab + export

--- components/Profile/Following.js
+++ components/Profile/Following.js
@@ -34,3 +34,73 @@ const Following = ({
       }
       setLoading(false);
     };
+
+    getFollowing();
+}, []);
+
+return (
+  <>
+    {loading ? (
+      <SpinEffect />
+    ) : following.length > 0 ? (
+      following.map((followingThisUser) => {
+        /*  */
+
+        const isFollowingUser =
+          loggedUserFollowers.following.length > 0 &&
+          loggedUserFollowers.following.filter(
+            (following) => following.user === followingThisUser.user._id
+          ).length > 0;
+
+        return (
+          <List
+            key={followingThisUser.user._id}
+            divided
+            verticalAlign="middle"
+          >
+            <List.Item>
+              <List.Content floated="right">
+                {followingThisUser.user._id !== user._id && (
+                  <Button
+                    color={isFollowingUser ? "linkify" : "connectdevelop"}
+                    icon={isFollowingUser ? "check" : "add user"}
+                    content={isFollowingUser ? "Following" : "Follow"}
+                    disabled={loadFollowInfo}
+                    onClick={() => {
+                      setLoadFollowInfo(true);
+
+                      isFollowingUser
+                        ? unfollow(
+                            followingThisUser.user._id,
+                            setUserFollowers
+                          )
+                        : follow(
+                            followingThisUser.user._id,
+                            setUserFollowers
+                          );
+
+                      setLoadFollowInfo(false);
+                    }}
+                  />
+                )}
+              </List.Content>
+              <Image avatar src={followingThisUser.user.dpLink} />
+              <List.Content
+                as="a"
+                href={`/${followingThisUser.user.username}`}
+              >
+                {followingThisUser.user.name}
+              </List.Content>
+            </List.Item>
+          </List>
+        );
+      })
+    ) : (
+      <NoFollowInfo followingTab={true} />
+    )}
+  </>
+);
+};
+
+export default Following;
+
