64a1c732f6ec3293b318fef6abec30169712d5ed
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-11-18 19:46:22 +0000

    ProfileHeader dependencies incl. semantic-ui react

---
+++ components/Profile/ProfileHeader.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import {
+  Segment,
+  Image,
+  Grid,
+  Divider,
+  Header,
+  Button,
+  List,
+} from "semantic-ui-react";
+import { follow, unfollow } from "../../utils/profileMethods";
