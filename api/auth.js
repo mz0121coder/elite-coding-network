ba0d6951abc56b73616ce669751ca7cc3d622939
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-18 01:50:43 +0000

    get auth routes

--- api/auth.js
+++ api/auth.js
@@ -5,3 +5,22 @@ const router = express.Router ();
 const UserModel = require ("..models/UserModel");
 const FollowerModel = require ("../models/FollowerModel");
 
+router.get ("/"), authMiddleware, async (req,res) =>{
+    const {userId} =req;
+
+    try {
+        const user = await UserModel.findById (userId);
+        if (!user) {
+            return res.status(404).send ("User Not Found");
+        }
+    }
+}
+
+const userFollowers = await FollowerModel.findOne({user:userId});
+
+return res.status (200).json({user, userFollowers });
+} catch (error) {
+    console.error(error);
+    return res.status(500.send ("Server Error");
+}
+});
