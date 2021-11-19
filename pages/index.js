67511b04c07aec7179bb2f77dbacf7bf461689fc
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-19 18:13:54 +0500

    index.js imports in pages subsection

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-function Index ({user, postsData, errorloading}) {
-    const [ posts, setPosts] useState (postsData || []);
-    const [showToastr, setShowToastr] = useState(false);
-    const [hasMore, setHasMore] = useState (true);
-}
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+import axios from "axios";
+import mainUrl from "../utils/mainUrl";
+import AddPost from "../components/Post/AddPost";
+import CardForPosts from "../components/Post/CardForPosts";
+import { Segment } from "semantic-ui-react";
+import { parseCookies } from "nookies";
+import { NoPosts } from "../components/Layout/NoData";
+import { PostDeleteToastr } from "../components/Layout/Toastr";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { phPosts, EndMsg } from "../components/Layout/PlaceHolders";
+import cookie from "js-cookie";
+import getUserInfo from "../utils/getUserInfo";
+import MsgAlertModal from "../components/Home/MsgAlertModal";
+import newMsgSound from "../utils/newMsgSound";
+import AlertWindows from "../components/Home/AlertWindows";
