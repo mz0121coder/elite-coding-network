8e9981d03fbe99dfed6228e5a7c285bcb1047c71
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-19 18:20:51 +0500

    Index function & useEffect(1) w/socket config

--- pages/index.js
+++ pages/index.js
@@ -15,3 +15,30 @@ import getUserInfo from "../utils/getUserInfo";
 import MsgAlertModal from "../components/Home/MsgAlertModal";
 import newMsgSound from "../utils/newMsgSound";
 import AlertWindows from "../components/Home/AlertWindows";
+
+function Index({ user, postsData, errorLoading }) {
+    const [posts, setPosts] = useState(postsData || []);
+    const [showToastr, setShowToastr] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+  
+    const [pageNumber, setPageNumber] = useState(2);
+  
+    const socket = useRef();
+  
+    const [newMsgReceivedAlert, setNewMsgReceived] = useState(null);
+    const [newMsgModal, showNewMsgModal] = useState(false);
+  
+    const [newAlert, setNewNotification] = useState(null);
+    const [alertPopup, showAlertPopup] = useState(false);
+  
+    useEffect(() => {
+      if (!socket.current) {
+        socket.current = io(mainUrl);
+      }
+  
+      if (socket.current) {
+        socket.current.emit("join", { userId: user._id });
+  
+        socket.current.on("newMsgReceived", async ({ newMsg }) => {
+          const { name, dpLink } = await getUserInfo(newMsg.sender);
+  
