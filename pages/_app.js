a1e4e03df1a4165e8c26b56df2d60e1201645ca4
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-19 18:05:54 +0500

    MyApp function, getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -6,3 +6,16 @@ import Layout from "../components/Layout/Layout";
 import "react-toastify/dist/ReactToastify.css";
 import "semantic-ui-css/semantic.min.css";
 import "cropperjs/dist/cropper.css";
+
+function MyApp({ Component, pageProps }) {
+    return (
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
+  
+  MyApp.getInitialProps = async ({ Component, ctx }) => {
+    const { token } = parseCookies(ctx);
+    let pageProps = {};
+  
