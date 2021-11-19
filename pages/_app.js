d459a79621d702951b835d268c4cee855df01a1e
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-19 18:05:04 +0500

    more _app.js imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,8 @@
 import axios from "axios";
-import {parseCookies, destroyCookie } from "nookies";
+import { parseCookies, destroyCookie } from "nookies";
+import mainUrl from "../utils/mainUrl";
+import { redirectUser } from "../utils/authUser";
+import Layout from "../components/Layout/Layout";
+import "react-toastify/dist/ReactToastify.css";
+import "semantic-ui-css/semantic.min.css";
+import "cropperjs/dist/cropper.css";
