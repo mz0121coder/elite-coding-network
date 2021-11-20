091281586f8bd809ea2f5969b665d34a4a6f4dcf
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-20 15:40:07 +0500

    comment tab frontend

--- components/Post/CommentTab.js
+++ components/Post/CommentTab.js
@@ -5,3 +5,31 @@ import { postComment } from "../../utils/postMethods";
 function CommentTab({ postId, user, setComments }) {
   const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
+
+  return (
+    <Form
+      reply
+      onSubmit={async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        await postComment(postId, user, text, setComments, setText);
+
+        setLoading(false);
+      }}
+    >
+      <Form.Input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add Comment"
+        action={{
+          color: "blue",
+          icon: "edit",
+          loading: loading,
+          disabled: text === "" || loading,
+        }}
+      />
+    </Form>
+  );
+}
+
+export default CommentTab;
