b3d26428ef7bc4a17d8836cc621ceaef883a4688
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-20 15:36:52 +0500

    imports for AddComments

---
+++ components/Post/AddComments.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { Comment, Icon } from "semantic-ui-react";
+import calcTime from "../../utils/calcTime";
+import { deleteComment } from "../../utils/postMethods";
