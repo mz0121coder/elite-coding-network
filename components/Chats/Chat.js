93fbf6827b8b8ae803a608153b4ce4f256791848
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-20 14:27:57 +0000

    add delete function for user

--- components/Chats/Chat.js
+++ components/Chats/Chat.js
@@ -3,3 +3,23 @@ import {Divider, Comment, Icon, List } from "semantic-ui-react";
 import {useRouter} from "next/router";
 import calcTime from "../..utils/calcTime";
 
+function Chat ({ chat, activeChats, deleteChats }) {
+    const router = useRouter();
+
+    const isOnline = 
+    activeChats.length > 0 &&
+    activeChats.filter((user) => user.userId === chat.msgsWithUser).length > 0;
+
+    return (
+    <>
+ <List selection>
+         <List.Item
+         active={router.query.message === chat.msgsWithUser}
+         onClick = {() =>
+         router.push(`/messages?messages=${chat.msgWithUser}`, undefined, {
+             shallow: true,
+         })
+        }
+    ></List.Item>    
+
+}
