eca7df072200cb48ae4d71ad428c85841816debd
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-20 16:35:00 +0000

    render search results with use effect

--- components/Chats/SearchChat.js
+++ components/Chats/SearchChat.js
@@ -1,3 +1,15 @@
-import React, {useState, UseEffect} from "react";
+import React, {useState, UseEffect, useEffect} from "react";
 import {List, Image, Search } from "semantic-ui-react";
 import axios from "axios";
+
+
+useEffect (() => {
+    <Search
+    onBlur={()} => {
+        results.length > 0 && setResults ([]);
+        loading && setLoading (false);
+        setText ("");
+    }}
+    
+    }
+}
