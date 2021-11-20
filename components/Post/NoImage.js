d30a63c693a7ff738e0dbaaba6e6c319eebd1e37
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-20 15:46:13 +0500

    NoImage imports + function setup

---
+++ components/Post/NoImage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Modal, Image, Card, Icon, Divider } from "semantic-ui-react";
+import AddComments from "./AddComments";
+import CommentTab from "./CommentTab";
+import calcTime from "../../utils/calcTime";
+import Link from "next/link";
+import { likePost } from "../../utils/postMethods";
+import LikesList from "./LikesList";
+
+function NoImage({
+  post,
+  user,
+  setLikes,
+  likes,
+  isLiked,
+  comments,
+  setComments,
+}) {
