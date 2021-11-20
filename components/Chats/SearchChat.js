f19f4108fab9ebffe90bb831644ab719cafaff8a
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-20 22:10:44 +0000

    add dependencies

--- components/Chats/SearchChat.js
+++ components/Chats/SearchChat.js
@@ -5,7 +5,13 @@ import cookie from "js-cookie";
 import {useRouter} from "next/router";
 import mainUrl from "../..utils.mainUrl";
 
-
+function SearchChat ({chats, setChats}) {
+    const [text, setText] = useState ("");
+    const [loading, setLoading] = useState (false);
+    const [results, setResults] = useState ([]);
+    
+    const RoUTER = useRouter ();
+}
 
 useEffect (() => {
     <Search
