ed8e198923dffaa9cece5ae5892c68451ca3d9e6
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-21 22:40:02 +0000

    add user links

--- components/Common/CommonFields.js
+++ components/Common/CommonFields.js
@@ -1,2 +1,16 @@
 import React from "react";
 
+function CommonFields ({
+    user: {bio, github, linkify, at, connectdevelop},
+    handleChange,
+    displayLinks,
+    setDisplayLinks,
+
+}) {
+    return (
+        <>
+        <Form.Field
+        required
+        control ={TextArea}
+    )
+}
