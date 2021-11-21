ca96dcf559316619668d3e9467d7e30ff77b3412
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-21 17:01:27 +0000

    add target value for search chats

--- components/Chats/SearchChat.js
+++ components/Chats/SearchChat.js
@@ -10,7 +10,46 @@ function SearchChat ({chats, setChats}) {
     const [loading, setLoading] = useState (false);
     const [results, setResults] = useState ([]);
     
-    const RoUTER = useRouter ();
+    const Router = useRouter ();
+
+    const handleChange =async (e) => {
+        const{ value } = e.target;
+        setText (value);
+        if (value.length === 0 ) return ;
+
+        setLoading (true);
+
+    try {
+        cancel && cancel ();
+        const CancelToken = axios.get(`${mainUrl}/api/search$(value}`, {
+            headers: {Authorization: token},
+            cancelToken: new CancelToken((canceler) => {
+                cancel = canceler;
+            }),
+        });
+
+        if (res.data.length === 0) {
+            results.length > 0 && setResults ([]);
+
+            return setLoading (false);
+        }
+
+        setResults (res.data);
+    } catch (error) {
+        console.log (error);
+    }
+    setLoading (false);
+};
+
+const addChat = (result) => {
+    const alreadyInChat =
+    chats.length > 0 &&
+    chats.filter ((chat) => chat.msgWithUser === result._id). length > 0;
+}
+
+    }
+     }
+    }
 }
 
 useEffect (() => {
