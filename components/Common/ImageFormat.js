78ddb282287717e0609688a696af7fc03531f94e
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-22 12:32:56 +0000

    add header sign up page

--- components/Common/ImageFormat.js
+++ components/Common/ImageFormat.js
@@ -1,3 +1,32 @@
 import React from "react";
 import { Form} from "semantic-ui-react";
 import{useRouter} from "next/router";
+
+function ImageFormat ({
+    highlight,
+    setHighlight,
+    inputRef,
+    handleChange,
+    mediaPreview,
+    setMedia,
+    dpLink,
+})  {
+    const router =useRouter ();
+
+    const signupRoute = rouer.pathname === "/signup";
+
+    const checkForSignupPage = () =>
+    signupRoute ? (
+        <>
+        <Header icon>
+            <icon
+            name="file image outline"
+            style={{ cursor: "pointer"}}
+            onClick={() => inputRef.current.click ()}
+            size = "huge"
+            />
+            Drag n Drop or Click to upload image
+        </Header>
+        </>
+    )
+}
