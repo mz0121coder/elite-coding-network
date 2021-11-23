22efc3bb39f228a5ef0ffda13bcf1997ef84cf9f
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 01:45:07 +0000

    set event to alert user of message

--- components/Home/MsgAlertModal.js
+++ components/Home/MsgAlertModal.js
@@ -3,3 +3,43 @@ import {Form, Modal, Segement, List, Icon} from "semantic-ui-react";
 import Link from "next/link";
 import calcTime from "../../utils/calcTime";
 
+
+function MsgAlertModal({
+    socket,
+    showNewMsgModal,
+    newMsgModal,
+    newMsgRecievedAlert,
+    user,
+}) {
+    const [text, setText] = useState("");
+    const [loading,setloading] = useState(false);
+
+    const onModalClose = () => showNewMsgModal (false);
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+        
+        if (socket.current) {
+            socket.current.emit ("sendMsgAlert", {
+                userId: user._id,
+                msgSendToUserId: newMsgRecievedAlert.sender,
+                msg:text,
+            });
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
