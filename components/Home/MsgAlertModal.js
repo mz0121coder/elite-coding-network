22c77796c18e296f3fde35b40d8d424c51aea608
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 01:34:26 +0000

    import dependencies

---
+++ components/Home/MsgAlertModal.js
@@ -0,0 +1,5 @@
+import React, {useState} from "react";
+import {Form, Modal, Segement, List, Icon} from "semantic-ui-react";
+import Link from "next/link";
+import calcTime from "../../utils/calcTime";
+
