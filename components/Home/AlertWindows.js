a171299dcf39449ad9b95a1107636ac9b89027ba
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 17:48:49 +0000

    add imports

--- components/Home/AlertWindows.js
+++ components/Home/AlertWindows.js
@@ -2,3 +2,20 @@ import { Segment, TransitionWindow, Icon, Feed} from "semantic-ui-react";
 import newMsgSound from "../../utils/newMsgSound";
 import { useRouter } from "next/router";
 import calcTime from "../../utils/calcTime";
+import MsgAlertModal from "./MsgAlertModal";
+
+function MsgAlertModal ({
+    socket,
+    showNewMsgModal,
+    newMsgModal,
+    newMsgRecievedAlert,
+    user,
+
+})  {
+    const [text, setText] = useState ("");
+    const [loading, setLoading] =useState (false);
+
+    const onModalClose = ()=> showNewMsgModal (false);
+
+    const formSumbit = 
+}
