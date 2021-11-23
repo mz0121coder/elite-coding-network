b5b37c938920aa58bba0f0f0933c86cc2990d1ef
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 20:02:33 +0000

    add verticle menu

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -7,3 +7,14 @@ function Navbar() {
   const router = useRouter();
 
   const isActive = route => router.pathname === route;
+
+
+  return (
+    <Menu fluid borderless>
+      <Container text>
+        <Link href="/login">
+          <Menu.Item header active={isActive("/login")}>
+            <Icon size="large" name="sign in" />
+            Login
+          </Menu.Item>
+        </Link>
