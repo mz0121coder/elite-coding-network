ecf9a64b47aa4eb68ef5960c5e4e66f5242a5bc9
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 20:51:28 +0000

    import dependencies and exports for user posts

---
+++ components/Layout/NoData.js
@@ -0,0 +1,12 @@
+import {Message, Button} from "semantic-ui-react";
+
+export const NoPosts = () => (
+    <>
+    <Message
+    info
+    icon ="code"
+    header ="No posts"
+    content="User has not posted anything yet!"
+    />
+    
+)
