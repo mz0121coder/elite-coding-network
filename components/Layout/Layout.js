6a73c6115d7d30ea0e4c0cb5531cc5e561799756
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 19:28:26 +0000

    responsive design + progress config

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -15,3 +15,20 @@ import SideBars from "./SideBars";
 import Search from "./Search";
 import ResponsiveHeader from "./ResponsiveHeader";
 import { createMedia } from "@artsy/fresnel";
+
+const AppMedia = createMedia({
+    breakpoints: { zero: 0, mobile: 549, tablet: 850, computer: 1080 },
+  });
+  
+  const mediaStyles = AppMedia.createMediaStyle();
+  const { Media, MediaContextProvider } = AppMedia;
+
+  function Layout({ children, user }) {
+    const contextRef = createRef();
+    const router = useRouter();
+  
+    const messagesRoute = router.pathname === "/messages";
+  
+    Router.onRouteChangeStart = () => nprogress.start();
+    Router.onRouteChangeComplete = () => nprogress.done();
+    Router.onRouteChangeError = () => nprogress.done();
