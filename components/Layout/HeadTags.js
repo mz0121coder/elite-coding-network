5813587d50145611f4e6af84dea85593035ca1f2
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 19:19:56 +0000

    import stylesheet/responsive

--- components/Layout/HeadTags.js
+++ components/Layout/HeadTags.js
@@ -1,5 +1,19 @@
 import head from "next/head";
-<>
-<Head>
-    <meta name = "vie"
-</Head>
+
+const HeadTags = () => (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.png" sizes="16*16" type="image/png" />
+  
+        <link rel="stylesheet" type="text/css" href="/listMessages.css" />
+  
+        <link rel="stylesheet" type="text/css" href="/styles.css" />
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+  
+        <title>Elite Coding Network</title>
+      </Head>
+    </>
+  );
+  export default HeadTags;
