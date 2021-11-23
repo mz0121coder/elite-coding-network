c2fbf1cf86fbba2ec6bb8b6bfb2e9680773c508f
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 00:39:28 +0000

    render header of social page

--- components/Common/WelcomeMsg.js
+++ components/Common/WelcomeMsg.js
@@ -1,3 +1,23 @@
 import { Icon, Message, Divider }  from "semantic-ui-react";
 import { useRouter } from "next/router";
 import Link from "next.link";
+
+export const HeaderMessage = () => {
+    const router = useRouter();
+    const signupRoute = router.pathname === "/signup";
+
+    return (
+        <Message
+        color = "teal"
+        attached
+        header = {signupRoute ? "Get Started" : "Welcome Back"}
+        icon = signupRoute ? "settings" : "privacy"}
+        content = {signupRoute ? "Create New Account": "Login with Email and Password"}
+        />
+    );
+    );
+
+    
+
+
+}
