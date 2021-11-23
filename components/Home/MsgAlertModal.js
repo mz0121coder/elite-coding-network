7b9a569870a5eebae21611b3e710f376f27a01d4
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-23 14:21:32 +0000

    alert instructions

--- components/Home/MsgAlertModal.js
+++ components/Home/MsgAlertModal.js
@@ -12,7 +12,7 @@ function MsgAlertModal({
     user,
 }) {
     const [text, setText] = useState("");
-    const [loading,setloading] = useState(false);
+    const [loading, setloading] = useState(false);
 
     const onModalClose = () => showNewMsgModal (false);
 
@@ -50,13 +50,87 @@ function MsgAlertModal({
                 <div className="inlineContainer">
                     <img 
                     className ="inlineIcon"
-                    src = {newMsgRecievedAlert.senderProfilePicture
+                    src = {newMsgRecievedAlert.senderProfilePicture}
                     />
                 </div> 
+
+                
+                <div className="otherBubble other">{newMsgReceivedAlert.msg}</div>
+
+                <span className="other">{calcTime(newMsgReceivedAlert.date)}</span>
+          </div>
    
+                <div style={{ position: "sticky", bottom: "0px" }}>
+                <Segment secondary color="teal" attached="bottom">
+                  <Form reply onSubmit={formSubmit}>
+                    <Form.Input
+                      size="large"
+                      placeholder="Send New Message"
+                      value={text}
+                      onChange={(e) => setText(e.target.value)}
+                      action={{
+                        color: "blue",
+                        icon: "telegram plane",
+                        disabled: text === "",
+                        loading: loading,
+                      }}
+                    />
+                  </Form>
+                </Segment>
+              </div>
+    
+              <div style={{ marginTop: "5px" }}>
+                <Link href={`/messages?message=${newMsgReceivedAlert.sender}`}>
+                  <a>View All Messages</a>
+                </Link>
+    
+                <br />
+    
+                <Instructions username={user.username} />
+              </div>
+            </Modal.Content>
+          </Modal>
+        </>
+      );
+    }
 
-    )
-}
+    const Instructions =({ Username}) = (
+      <List>
+        <List.Item>
+          <Icon name="help" />
+          <List.Content>
+            <List.Header>
+              If you do not like this popup tp appear when you recieve a new message:
+            </List.Header>
+          </List.Content>
+        </List.Item>
+
+        <List.Item>
+          <Icon name = "hand point right" />
+          <List.Content>You can disable it by going to <Link href={`${username}`}>
+            <a> Account </a>
+            </Link>
+            Page and clicking on Setting Tab.
+            </List.Content> 
+        </List.Item>
+
+        <List.Item>
+          <Icon name ="hand point right" />
+          Inside the menu, there is a setting named...............
+        </List.Item>
+
+        <List.Item>
+          <Icon name ="hand point right" />
+          Just toggle ddddddddddddddddddddddddddddddddddddddddd.
+        </List.Item>
+        
+
+      </List>
+
+    );
+
+    export default MsgAlertModal;
+    
 
 
 
@@ -70,5 +144,4 @@ function MsgAlertModal({
 
 
 
-
-
+s
