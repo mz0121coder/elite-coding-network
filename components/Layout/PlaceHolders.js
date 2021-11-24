d48a193940e9efa2fdac1983f5c5104b351eb1f4
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-24 18:51:50 +0000

    placeholder for suggestions & alerts/notifications

--- components/Layout/PlaceHolders.js
+++ components/Layout/PlaceHolders.js
@@ -28,3 +28,46 @@ export const phPosts = () =>
       <Divider hidden />
     </div>
   ));
+
+export const phSuggestions = () => (
+  <>
+    <List.Item>
+      <Card color="red">
+        <Placeholder>
+          <Placeholder.Image square />
+        </Placeholder>
+        <Card.Content>
+          <Placeholder>
+            <Placeholder.Header>
+              <Placeholder.Line length="medium" />
+            </Placeholder.Header>
+          </Placeholder>
+        </Card.Content>
+
+        <Card.Content extra>
+          <Button
+            disabled
+            circular
+            size="small"
+            icon="add user"
+            content="Follow"
+            color="connectdevelop"
+          />
+        </Card.Content>
+      </Card>
+    </List.Item>
+  </>
+);
+
+export const phAlerts = () =>
+  range(1, 10).map((item) => (
+    <>
+      <Placeholder key={item}>
+        <Placeholder.Header image>
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Header>
+      </Placeholder>
+      <Divider hidden />
+    </>
+  ));
