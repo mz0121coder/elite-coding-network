7069ab78a2178a689e5158ee6b2154ffe2ff2cd1
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-24 18:41:23 +0000

    function with useRouter

--- components/Layout/ResponsiveHeader.js
+++ components/Layout/ResponsiveHeader.js
@@ -3,3 +3,10 @@ import { Menu, Container, Icon, Dropdown } from "semantic-ui-react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { logoutUser } from "../../utils/authUser";
+
+function ResponsiveHeader({
+    user: { unreadNotification, email, unreadMsg, username },
+  }) {
+    const router = useRouter();
+    const isActive = (route) => router.pathname === route;
+  
