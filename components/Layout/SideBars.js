5b1f912c1edf866645c54b12ee40a17482c85f64
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-24 18:47:41 +0000

    SideBars imports

---
+++ components/Layout/SideBars.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { List, Icon } from "semantic-ui-react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { logoutUser } from "../../utils/authUser";
