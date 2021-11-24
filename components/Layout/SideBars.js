2c996af2027c0ecb6d5733e3ef47e8725b9e4e6a
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-24 18:48:04 +0000

    SideBars function / params

--- components/Layout/SideBars.js
+++ components/Layout/SideBars.js
@@ -3,3 +3,12 @@ import { List, Icon } from "semantic-ui-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { logoutUser } from "../../utils/authUser";
+
+function SideBars({
+    user: { unreadNotification, email, unreadMsg, username },
+    pc = true,
+  }) {
+    const router = useRouter();
+  
+    const isActive = (route) => router.pathname === route;
+  
