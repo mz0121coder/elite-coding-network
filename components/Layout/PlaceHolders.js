7e6177aeb8883b5cdb5477306050df231c9916db
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-24 18:50:44 +0000

    Placeholder for posts

--- components/Layout/PlaceHolders.js
+++ components/Layout/PlaceHolders.js
@@ -9,3 +9,22 @@ import {
   Icon,
 } from "semantic-ui-react";
 import { range } from "lodash";
+
+export const phPosts = () =>
+  range(1, 3).map((item) => (
+    <div key={item}>
+      <Placeholder fluid>
+        <Placeholder.Header image>
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Header>
+        <Placeholder.Paragraph>
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Paragraph>
+      </Placeholder>
+      <Divider hidden />
+    </div>
+  ));
