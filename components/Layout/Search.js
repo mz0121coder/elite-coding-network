7927ebcf992500ac2f3d70d6dbd52bf7fdf31941
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-24 18:43:51 +0000

    Try/catch, cancel searches as we get more specific

--- components/Layout/Search.js
+++ components/Layout/Search.js
@@ -21,3 +21,28 @@ function SearchTab() {
         setText(value);
         setLoading(true);
     
+        try {
+            cancel && cancel();
+            const CancelToken = axios.CancelToken;
+            const token = cookie.get("token");
+      
+            const res = await axios.get(`${mainUrl}/api/search/${value}`, {
+              headers: { Authorization: token },
+              cancelToken: new CancelToken((canceler) => {
+                cancel = canceler;
+              }),
+            });
+      
+            if (res.data.length === 0) {
+              results.length > 0 && setResults([]);
+      
+              return setLoading(false);
+            }
+            setResults(res.data);
+          } catch (error) {
+            console.log(error);
+          }
+      
+          setLoading(false);
+        };
+      
