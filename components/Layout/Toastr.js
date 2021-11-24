a52eb495f33bb5ff0ce17860c7c6d529b346a725
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-24 18:47:08 +0000

    Toastr config (msg sent successfully)

--- components/Layout/Toastr.js
+++ components/Layout/Toastr.js
@@ -51,3 +51,27 @@ export const ErrorToastr = ({ error }) => {
     </ToastContainer>
   );
 };
+
+export const MsgSentToastr = () => (
+  <ToastContainer
+    position="bottom-center"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover={false}
+  >
+    {toast.success("Sent successfully", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+    })}
+  </ToastContainer>
+);
